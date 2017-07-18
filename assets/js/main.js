9744a6cee20c01c68246726748473d14965ff30b
franshu <fran.ojeda208@gmail.com>
2017-07-18 14:57:02 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	//1.al hacer click en un contacto debe mostrar su chat en la parte superior del panel derecho
+	$('#contactos').on('click',function({
+
+		contactos.push('chat-enCurso') 
+
+	});
+
+	//tomar del imput
+	$('#chat-imput').append("dashboard");
+});
+
+
+
